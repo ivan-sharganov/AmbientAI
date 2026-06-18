6cbd854e08c0cc06fe26afd5464cda8811fbf675
Ivan Sharganov <sharganov.ivan@gmail.com>
2026-06-18 14:14:56 +0300

    Initial Commit

---
+++ AmbientAI/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AmbientAI
+//
+//  Created by Ivan Sharganov on 18.06.2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
